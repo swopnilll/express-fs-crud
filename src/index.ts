151d14130369ae49bd4079021f6fdbc487999cfc
Swopnil <leapfrog@pop-os.localdomain>
2022-09-25 15:40:43 +0545

    express-0: First Hello World Server in Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import express, { Application } from 'express';
 
+const app: Application = express();
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello first server </h1>")
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log("server is active"));
